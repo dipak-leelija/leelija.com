0f1b9808fc06cbd72554616fef993f7e587feaf3
Rozy <rozybegum.leelija@gmail.com>
2023-07-18 12:01:30 +0530

    datatables entries series changes

--- leelija-admin/employees.php
+++ leelija-admin/employees.php
@@ -36,7 +36,7 @@ $page = "Admin_employees";
                         </div>
                         <div class="card-body px-0 pt-0 pb-2">
                             <div class="table-responsive p-0">
-                                <table class="table align-items-center mb-0 datatable" id="employees_tables">
+                                <table class="table align-items-center mb-0 datatable">
                                     <thead>
                                         <tr>
                                             <th
