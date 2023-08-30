29201314138d3a736682a4e2a700fbd9eaecc4bd
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-08-30 14:19:18 +0530

    error reporting enabled

--- includes/constant.inc.php
+++ includes/constant.inc.php
@@ -1,5 +1,9 @@
 <?php 
-	
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 	date_default_timezone_set("Asia/Calcutta");   //India time (GMT+5:30)
 	define("NOW", 			date("Y-m-d H:i:s"));
 
