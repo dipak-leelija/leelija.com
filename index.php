8a1c3d5327f4afe20bef664ba556f644c79340bb
Rozy <rozybegum.leelija@gmail.com>
2023-09-13 14:38:13 +0530

    leelija index page section added

--- index.php
+++ index.php
@@ -97,6 +97,46 @@ $cusId		= $utility->returnSess('userid', 0);
 
         <!----------------------------------------- Main Banner End ----------------------------------------->
 
+        <!-- _____________________________________________________________________________________ -->
+        <!-- Trusted by top brands & SEO agencies for first page results SLIDER -->
+        <section class="clientsides-item" id="clientslogo">
+            <div class="containering">
+                <div>
+                    <h2 class="px-3 px-md-0">Our clients earned brand mentions from <strong>publications
+                            like...</strong></h2>
+                </div>
+
+                <div class="row clientside-logo-row">
+                    <div class="clientsides-logo-wrap col-sm-6 col-md-3">
+                        <img src="images/clients-logos/cc1.png" class="cslogo-img-responsiving w-100" alt="">
+                    </div>
+                    <div class="clientsides-logo-wrap  col-sm-6 col-md-3">
+                        <img src="images/clients-logos/khatabook3.png" class="cslogo-img-responsiving" alt="">
+                    </div>
+                    <div class="clientsides-logo-wrap  col-sm-6 col-md-3">
+                        <img src="images/clients-logos/specscart3.png " class="cslogo-img-responsiving" alt="">
+                    </div>
+                    <div class="clientsides-logo-wrap col-sm-6 col-md-3 ">
+                        <img src="images/clients-logos/namecheap1.png" class="cslogo-img-responsiving " alt="">
+                    </div>
+                    <div class="clientsides-logo-wrap col-sm-6 col-md-3">
+                        <img src="images/clients-logos/cs1.png" class="cslogo-img-responsiving" alt="">
+                    </div>
+                    <div class="clientsides-logo-wrap  col-sm-6 col-md-3">
+                        <img src="images/clients-logos/icademy1.png" class="cslogo-img-responsiving" alt="">
+                    </div>
+                    <div class="clientsides-logo-wrap  col-sm-6 col-md-3">
+                        <img src="images/clients-logos/ISB3.png" class="cslogo-img-responsiving" alt="">
+                    </div>
+                    <div class="clientsides-logo-wrap  col-sm-6 col-md-3 ">
+                        <img src="images/clients-logos/recovery3.png" class="cslogo-img-responsiving" alt="">
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- Trusted by top brands & SEO agencies for first page results SLIDER -->
+
         <!--==================================== Established Blogs Start ====================================-->
         <?php // require_once 'components/main/items-card.php'; ?>
         <!--------------------------------------- Established Blogs End --------------------------------------->
@@ -244,7 +284,7 @@ $cusId		= $utility->returnSess('userid', 0);
             <div class="row aBout_us-banner-section">
                 <h1>About Us</h1>
                 <div class="col-md-6 text-center text-md-start ps-md-0 ">
-                    <img src="images/aboutus_banner-home2.jpg"  alt="">
+                    <img src="images/aboutus_banner-home2.jpg" alt="">
                 </div>
                 <div class="col-md-6 m-auto">
 
@@ -271,7 +311,7 @@ $cusId		= $utility->returnSess('userid', 0);
 
         <!-- ================================ OUR TEAM  =================================== -->
 
-        
+
         <!-- ============================== OUR TEAM END ================================= -->
 
 
