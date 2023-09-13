5dc86c5ecefa7a29a03997c9365f1c8c17282249
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-09-13 17:55:40 +0530

    pagination separeted

--- seller/myblogs.php
+++ seller/myblogs.php
@@ -69,7 +69,7 @@ $domainDtls		= $domain->ShowUserDomainData($cusDtl[0][2]);
     <link href="<?= URL ?>assets/portal-assets/plugins/perfectscroll/perfect-scrollbar.css" rel="stylesheet">
     <link href="<?= URL ?>assets/portal-assets/plugins/pace/pace.css" rel="stylesheet">
     <link href="<?= URL ?>assets/portal-assets/plugins/highlight/styles/github-gist.css" rel="stylesheet">
-    <link href="<?= URL ?>assets/portal-assets/plugins/datatables/datatables.min.css" rel="stylesheet">
+    <link href="<?= URL ?>assets/vendors/pagination/pagination.css" rel="stylesheet" >
 
 
     <!-- Theme Styles -->
@@ -98,16 +98,16 @@ $domainDtls		= $domain->ShowUserDomainData($cusDtl[0][2]);
                         <div class="row">
                             <div class="col">
                                 <div class="card  px-3 py-5">
-                                    <section class="gallery">
+                                    <section class="">
                                         <!-- row start -->
-                                        <div class="row gallery-items">
-                                            <!-- col start -->
+                                        <div class="row justify-content-center px-3">
+
                                             <?php
-					                               if ($domainDtls != NULL) {
-						                            foreach($domainDtls as $eachRecord){
-								                    $nicheDtls	 	= $Niche->showBlogNichMst($eachRecord['niche']);
-						                          ?>
-                                            <div class="item col-12 col-sm-6 col-md-4">
+					                        if ($domainDtls != NULL) {
+						                        foreach($domainDtls as $eachRecord){
+								                $nicheDtls	 	= $Niche->showBlogNichMst($eachRecord['niche']);
+						                    ?>
+                                            <div class="item col-12 col-sm-6 col-md-4 px-4">
 
                                                 <div class="wrapping_div-blogcard">
                                                     <div class="product-img">
@@ -156,27 +156,27 @@ $domainDtls		= $domain->ShowUserDomainData($cusDtl[0][2]);
                                                 </div>
                                             </div>
                                             <?php
-							                          }
-						                             }
-						                            ?>
+							                    }
+						                    }
+						                    ?>
                                         </div>
                                         <!-- row end -->
-                                        <div class="d-flex justify-content-end mt-3">
+                                        <!-- <div class="d-flex justify-content-end mt-3">
                                             <nav aria-label="Page navigation">
                                                 <ul class="pagination">
                                                     <li class="prev page-item"><a class="page-link"
                                                             href="#">Previous</a>
                                                     </li>
-                                                    <!-- <div id="pageNums"> -->
+                                                    <!-- <div id="pageNums"> 
                                                     <li class="page page-item">
                                                         <a class="page-link"> <span class="page-num"></a>
                                                     </li>
-                                                    <!-- </div> -->
+                                                    <!-- </div>
                                                     <li class="next page-item"><a class="page-link" href="#">Next</a>
                                                     </li>
                                                 </ul>
                                             </nav>
-                                        </div>
+                                        </div> -->
                                     </section>
                                 </div>
 
@@ -198,214 +198,10 @@ $domainDtls		= $domain->ShowUserDomainData($cusDtl[0][2]);
         <script src="<?= URL ?>assets/portal-assets/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
         <script src="<?= URL ?>assets/portal-assets/plugins/pace/pace.min.js"></script>
         <script src="<?= URL ?>assets/portal-assets/plugins/highlight/highlight.pack.js"></script>
-        <script src="<?= URL ?>assets/portal-assets/plugins/datatables/datatables.min.js"></script>
         <script src="<?= URL ?>assets/portal-assets/js/main.min.js"></script>
-        <script src="<?= URL ?>assets/portal-assets/js/pages/datatables.js"></script>
-
-        <!-- <script>
-    /* jQuery Pagination */
-    (function($) {
-
-        var paginate = {
-            startPos: function(pageNumber, perPage) {
-
-                return pageNumber * perPage;
-            },
-
-            getPage: function(items, startPos, perPage) {
-
-                var page = [];
-
-
-                items = items.slice(startPos, items.length);
-
-
-                for (var i = 0; i < perPage; i++) {
-                    page.push(items[i]);
-                }
-
-                return page;
-            },
-
-            totalPages: function(items, perPage) {
-
-                return Math.ceil(items.length / perPage);
-            },
-
-            createBtns: function(totalPages, currentPage) {
-
-                var pagination = $('<div class="custom_pagination" />');
-
-
-                pagination.append('<span class="custom_pagination-button">&laquo;</span>');
-
-
-                for (var i = 1; i <= totalPages; i++) {
-
-                    if (totalPages > 5 && currentPage !== i) {
-
-                        if (currentPage === 1 || currentPage === 2) {
-
-                            if (i > 5) continue;
-
-                        } else if (currentPage === totalPages || currentPage === totalPages - 1) {
-
-                            if (i < totalPages - 4) continue;
-
-                        } else {
-                            if (i < currentPage - 2 || i > currentPage + 2) {
-                                continue;
-                            }
-                        }
-                    }
-
-
-                    var pageBtn = $('<span class="custom_pagination-button page-num" />');
-
-
-                    if (i == currentPage) {
-                        pageBtn.addClass('active');
-                    }
-
-
-                    pageBtn.text(i);
-
-
-                    pagination.append(pageBtn);
-                }
-
-
-                pagination.append($('<span class="custom_pagination-button">&raquo;</span>'));
-
-                return pagination;
-            },
-
-            createPage: function(items, currentPage, perPage) {
-
-                $('.custom_pagination').remove();
-
-
-                var container = items.parent(),
-
-                    items = items.detach().toArray(),
-
-                    startPos = this.startPos(currentPage - 1, perPage),
-                    page = this.getPage(items, startPos, perPage);
-
-
-                $.each(page, function() {
-
-                    if (this.window === undefined) {
-                        container.append($(this));
-                    }
-                });
-
-                var totalPages = this.totalPages(items, perPage),
-                    pageButtons = this.createBtns(totalPages, currentPage);
-
-                container.after(pageButtons);
-            }
-        };
-
-        $.fn.paginate = function(perPage) {
-            var items = $(this);
-
-            if (isNaN(perPage) || perPage === undefined) {
-                perPage = 5;
-            }
-
-
-            if (items.length <= perPage) {
-                return true;
-            }
-
-            if (items.length !== items.parent()[0].children.length) {
-                items.wrapAll('<div class="custom_pagination-items" />');
-            }
-
-            paginate.createPage(items, 1, perPage);
-
-
-            $(document).on('click', '.custom_pagination-button', function(e) {
-                var currentPage = parseInt($('.custom_pagination-button.active').text(), 10),
-                    newPage = currentPage,
-                    totalPages = paginate.totalPages(items, perPage),
-                    target = $(e.target);
-
-                newPage = parseInt(target.text(), 10);
-                var i = currentPage;
-                i <= totalPages;
-                i++;
-                if (target.text() == '»') newPage = i++;
-                i--;
-                if (target.text() == '«') newPage = --i;
-                if (newPage > 0 && newPage <= totalPages) {
-                    paginate.createPage(items, newPage, perPage);
-                }
-            });
-        };
-
-    })(jQuery);
-    $('.wrapping_div-blogcard').paginate(3);
-    </script> -->
-
+        <script src="<?= URL ?>assets/vendors/pagination/pagination.js"></script>
         <script>
-        const galleryItems = document.querySelector(".gallery-items").children;
-        const prev = document.querySelector(".prev");
-        const next = document.querySelector(".next");
-        const page = document.querySelector(".page-num");
-        const maxItem = 3;
-        let index = 1;
-
-        const pagination = Math.ceil(galleryItems.length / maxItem);
-
-        prev.addEventListener("click", function() {
-            index--;
-            check();
-            showItems();
-        })
-        next.addEventListener("click", function() {
-            index++;
-            check();
-            showItems();
-        })
-
-        function check() {
-            if (index == pagination) {
-                next.classList.add("disabled");
-            } else {
-                next.classList.remove("disabled");
-            }
-
-            if (index == 1) {
-                prev.classList.add("disabled");
-            } else {
-                prev.classList.remove("disabled");
-            }
-        }
-
-        function showItems() {
-            for (let i = 0; i < galleryItems.length; i++) {
-                galleryItems[i].classList.remove("show");
-                galleryItems[i].classList.add("hide");
-
-
-                if (i >= (index * maxItem) - maxItem && i < index * maxItem) {
-                    // if i greater than and equal to (index*maxItem)-maxItem;
-                    // means  (1*8)-8=0 if index=2 then (2*8)-8=8
-                    galleryItems[i].classList.remove("hide");
-                    galleryItems[i].classList.add("show");
-                }
-                page.innerHTML = index;
-            }
-
-
-        }
-
-        window.onload = function() {
-            showItems();
-            check();
-        }
+        $('.item').paginate(3);
         </script>
 </body>
 
