1d142f0755eacabbf5a6734c8a69be71fec18ff7
Dipak Majumdar <dipakmajumdar.leelija@gmail.com>
2023-11-02 17:43:03 +0530

    registration constant created

---
+++ includes/registration.inc.php
@@ -0,0 +1,58 @@
+<?php 
+	/**
+	*	Constants defined for new user registration
+	*
+	*	@author		Dipak Majumdar
+	*	@date		
+	*	@update		
+	*	@copyright	Leelija Web Solutions Pvt. Ltd.
+	*	@url		http://www.leelija.com
+	*	@email		dipakmajumdar.leelija@gmail.com
+	* 
+	*/
+	
+	/**
+	*	Generic message for registration 
+	*/
+
+	//first name & last name
+	define('ERREG000', ' Name or contact person field is empty');
+	define('ERREG001', ' First Name is empty');
+	define('ERREG002', ' Surname is empty');
+	define('ERREG013', ' Select your category');
+	define('ERREG015', ' Select Customer Type');
+
+	//contact + address
+	define('ERREG003', ' Phone number is empty');
+	define('ERREG004', ' Address is empty');
+	define('ERREG005', ' Invalid email id');
+	define('ERREG006', ' Email already exists');
+	define('ERREG007', ' Organization or business name is empty');
+	define('ERREG008', ' Designation can not be left empty');
+	define('ERREG009', ' Suburb or City or Town is empty');
+	define('ERREG010', ' District is empty');
+	define('ERREG011', ' State is empty');
+	define('ERREG012', ' Town is empty');
+	// define('ERU117', 	'Password Length is short');
+	define('ERREG014', ' Phone number already exits');
+	define('conp0001', 	'Password Not Matched');
+
+	
+	//bank account related
+	define('ERREG101', ' UTR or CIS is not valid');
+	define('ERREG102', ' Bank name is empty');
+	define('ERREG103', ' Account number is empty'); 
+	define('ERREG104', ' NI number is empty.');
+	define('ERREG105', ' Sort code is empty.');
+	
+	
+	//user agreement
+	define('ERREG201', ' Please check the user agreement');
+	
+	
+	//success message
+	define('SUREG001', ' Thanks for registration!! Now You can login And Enjoy with us');
+	define('SUREG002', ' Success !!! Your registration is forwarded to our account section. We are verifying your access. Once verified you will be able to login.');
+	define('SUREG003', ' Success !!! Your request to become a part of our business has been submitted. Our marketing team will get back to you soon.');
+	
+?>
